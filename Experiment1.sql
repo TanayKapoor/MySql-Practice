fa7d0b5d3f6b02f2fad0efd5230d945768b59e45
TanayKapoor <itstkaay@gmail.com>
2020-12-19 14:49:08 +0530

    Added Experiment 1

---
+++ Experiment1.sql
@@ -0,0 +1,29 @@
+/* Create Command */
+CREATE DATABASE experiment;
+
+/* Create table command */
+CREATE TABLE student (
+    roll_number int,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+/* Alter Command */
+
+ALTER TABLE student
+ADD DateOfBirth date;
+
+ALTER TABLE student
+ALTER COLUMN DateOfBirth year;
+
+ALTER TABLE student
+DROP COLUMN DateOfBirth;
+
+/* Truncate Command */
+TRUNCATE TABLE student;
+
+/* DROP */
+DROP TABLE student;
+DROP DATABASE experiment;
