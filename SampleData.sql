7312b32cc1c4a5121d329698eac6b6b3d22ffe69
TanayKapoor <itstkaay@gmail.com>
2020-12-20 19:50:50 +0530

    Added Sample data

---
+++ SampleData.sql
@@ -0,0 +1,115 @@
+/* CREATING DB */
+
+CREATE TABLE regions (
+	region_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+	region_name VARCHAR (25) DEFAULT NULL
+);
+
+CREATE TABLE countries (
+	country_id CHAR (2) PRIMARY KEY,
+	country_name VARCHAR (40) DEFAULT NULL,
+	region_id INT (11) NOT NULL,
+	FOREIGN KEY (region_id) REFERENCES regions (region_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE locations (
+	location_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+	street_address VARCHAR (40) DEFAULT NULL,
+	postal_code VARCHAR (12) DEFAULT NULL,
+	city VARCHAR (30) NOT NULL,
+	state_province VARCHAR (25) DEFAULT NULL,
+	country_id CHAR (2) NOT NULL,
+	FOREIGN KEY (country_id) REFERENCES countries (country_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE jobs (
+	job_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+	job_title VARCHAR (35) NOT NULL,
+	min_salary DECIMAL (8, 2) DEFAULT NULL,
+	max_salary DECIMAL (8, 2) DEFAULT NULL
+);
+
+CREATE TABLE departments (
+	department_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+	department_name VARCHAR (30) NOT NULL,
+	location_id INT (11) DEFAULT NULL,
+	FOREIGN KEY (location_id) REFERENCES locations (location_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE employees (
+	employee_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR (20) DEFAULT NULL,
+	last_name VARCHAR (25) NOT NULL,
+	email VARCHAR (100) NOT NULL,
+	phone_number VARCHAR (20) DEFAULT NULL,
+	hire_date DATE NOT NULL,
+	job_id INT (11) NOT NULL,
+	salary DECIMAL (8, 2) NOT NULL,
+	manager_id INT (11) DEFAULT NULL,
+	department_id INT (11) DEFAULT NULL,
+	FOREIGN KEY (job_id) REFERENCES jobs (job_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (department_id) REFERENCES departments (department_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (manager_id) REFERENCES employees (employee_id)
+);
+
+CREATE TABLE dependents (
+	dependent_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR (50) NOT NULL,
+	last_name VARCHAR (50) NOT NULL,
+	relationship VARCHAR (25) NOT NULL,
+	employee_id INT (11) NOT NULL,
+	FOREIGN KEY (employee_id) REFERENCES employees (employee_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/* LOADING DATA */
+INSERT INTO regions(region_id,region_name) VALUES (1,'Europe');
+INSERT INTO regions(region_id,region_name) VALUES (2,'Americas');
+INSERT INTO regions(region_id,region_name) VALUES (3,'Asia');
+INSERT INTO regions(region_id,region_name) VALUES (4,'Middle East and Africa');
+
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('AR','Argentina',2);
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('AU','Australia',3);
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('BE','Belgium',1);
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('BR','Brazil',2);
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('CA','Canada',2);
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('CH','Switzerland',1);
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('CN','China',3);
+INSERT INTO countries(country_id,country_name,region_id) VALUES ('DE','Germany',1);
+
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (1400,'2014 Jabberwocky Rd','26192','Southlake','Texas','US');
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (1500,'2011 Interiors Blvd','99236','South San Francisco','California','US');
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (1700,'2004 Charade Rd','98199','Seattle','Washington','US');
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (1800,'147 Spadina Ave','M5V 2L7','Toronto','Ontario','CA');
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (2400,'8204 Arthur St',NULL,'London',NULL,'UK');
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id) VALUES (2500,'Magdalen Centre, The Oxford Science Park','OX9 9ZB','Oxford','Oxford','UK');
+
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (1,'Public Accountant',4200.00,9000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (2,'Accounting Manager',8200.00,16000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (3,'Administration Assistant',3000.00,6000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (4,'President',20000.00,40000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (5,'Administration Vice President',15000.00,30000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (6,'Accountant',4200.00,9000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (7,'Finance Manager',8200.00,16000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (8,'Human Resources Representative',4000.00,9000.00);
+INSERT INTO jobs(job_id,job_title,min_salary,max_salary) VALUES (9,'Programmer',4000.00,10000.00);
+
+INSERT INTO departments(department_id,department_name,location_id) VALUES (1,'Administration',1700);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (2,'Marketing',1800);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (3,'Purchasing',1700);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (4,'Human Resources',2400);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (5,'Shipping',1500);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (6,'IT',1400);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (7,'Public Relations',2700);
+
+
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (1,'Penelope','Gietz','Child',206);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (2,'Nick','Higgins','Child',205);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (3,'Ed','Whalen','Child',200);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (4,'Jennifer','King','Child',100);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (5,'Johnny','Kochhar','Child',101);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (6,'Bette','De Haan','Child',102);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (7,'Grace','Faviet','Child',109);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (8,'Matthew','Chen','Child',110);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (9,'Joe','Sciarra','Child',111);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (10,'Christian','Urman','Child',112);
+INSERT INTO dependents(dependent_id,first_name,last_name,relationship,employee_id) VALUES (11,'Zero','Popp','Child',113);
